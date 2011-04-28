4d53545d247295a312ce9f6dbbbd9673c796fcd1
Brian Zollinhofer <brian.zollinhofer@gmail.com>
2011-04-28 09:13:00 -0400

    Added user following

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 TutApp::Application.routes.draw do
   get "sessions/new"
 
-	resources :users
-	resources :sessions, 		:only => [:new, :create, :destroy]  
-	resources :microposts,	:only => [:create, :destroy]
+	resources :users do
+		member do
+			get :following, :followers
+		end
+	end
+	resources :sessions, 			:only => [:new, :create, :destroy]  
+	resources :microposts,		:only => [:create, :destroy]
+	resources :relationships, :only => [:create, :destroy]
+
 	get "users/new"
 
   match '/signup', 		:to => 'users#new'
